a92d597525694b5622405ea374300183dbed1ee7
datuanho2k9 <datuanho2k9@gmail.com>
2021-11-09 22:43:42 +0700

    Add.js to add data

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,20 +1,19 @@
 const functions = require("firebase-functions");
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
+const admin = require("firebase-admin");
+admin.initializeApp();
 // Use cors for app
 app.use(cors({ origin: true }));
 // Use bodyparser for app
 app.use(bodyParser.json());
+
+const add = require('./src/add');
+
+app.use('/', add);
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
---
+++ backend/functions/src/add.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const admin = require('firebase-admin');
+router.post('/add', (req, res) => {
+    // Expected piece of data:
+    // {
+    //     "identity": {
+    //         "name": "string" otherwise null, 
+    //     },
+    //     "content": {
+    //         "wish": "string",
+    //         "teacherName": "string"
+    //     }
+    // }
+
+    console.log(req.body);
+
+    // Escape everything in the data
+    const data = JSON.parse(JSON.stringify(req.body));
+    
+    checkTeacherName(data.content.teacherName, () => {
+        add(data);
+    });
+   
+    
+    res.send('ok')
+})
+
+
+// Check if teacher's name exists in Content firebase collection as a function with paramenter teacherName
+// If it does, then close function
+// If it doesn't, create one
+function checkTeacherName(teacherName, callback) {
+    admin.firestore().collection('Content').doc(teacherName).get().then(doc => {
+        if (doc.exists) {
+            console.log('Teacher name exists');
+            callback()
+        } else {
+            console.log('Teacher name does not exist');
+            admin.firestore().collection('Content').doc(teacherName).set({
+                wishes: [],
+            })
+            callback()
+        }
+    })
+}
+
+function add(data){
+    admin.firestore().collection('Content').doc(data.content.teacherName).update({
+        wishes: admin.firestore.FieldValue.arrayUnion({
+            name: data.identity.name,
+            wish: data.content.wish
+        })
+    })
+}
+
+module.exports = router;
